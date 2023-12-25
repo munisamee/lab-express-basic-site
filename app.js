fc1eed3bb61510d5dfe09f8d1157d80e38fffb4f
Munisakhon Ermatova <munisakhonermatova@munisakhons-air.speedport.ip>
2023-12-25 18:42:47 +0100

    Lab halfway solved

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')// loads the express package
+const app = express() // create the express server
+const port = 3000 // define the express port
+
+app.use(express.static('public')) // tells express where the static files are located
+// Homepage Route
+app.get('/', (req, res) => {
+    console.log(__dirname)
+    res.sendFile(`${__dirname}/views/index.html`)
+})
+
+// About page route
+app.get('/about', (req, res) => {
+    res.sendFile(`${__dirname}/views/about.html`)
+})
+
+// Albums page route
+app.get('/albums', (req, res) => {
+    res.sendFile(`${__dirname}/views/albums.html`)
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
